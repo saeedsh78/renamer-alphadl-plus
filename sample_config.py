c7b780a88c7b08de192291f700b64605499e1c03
saeedsh78 <45710959+saeedsh78@users.noreply.github.com>
2022-01-23 00:37:14 +0330

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -14,6 +14,7 @@ class Config(object):
     CHAT_ID = os.environ.get("CHAT_ID", "")
     # Array to store users who are authorized to use the bot
     AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
+    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "").split())
     # Banned Unwanted Members..
     BANNED_USERS = []
     # the download location, where the HTTP Server runs
